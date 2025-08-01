a0c05140d8fe539587f87989417c20f2e77bd55c
chayaclass <chayanclass@gmail.com>
2025-08-01 04:30:45 -0700

    change config

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,6 +18,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
   ]
 
 });
